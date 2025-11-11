3bd4f025eff3027f55cc5b36b6aef118581cf873
Sharmeen <sharmeenmirzaalbaig@gmail.com>
2025-11-11 20:34:19 +0300

    sign-out routes

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,6 @@ const authCtrl = require("../controllers/auth")
 
 router.get("/sign-in", authCtrl.auth_signin_get)
 router.post("/sign-in", authCtrl.auth_signin_post)
-
+router.get('/sign-out', authCtrl.auth_signout_get)
 
 module.exports = router
