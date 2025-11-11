0e2d92539a578bf30e325bb7f28c3f67755dc72a
Sharmeen <sharmeenmirzaalbaig@gmail.com>
2025-11-11 20:43:30 +0300

    server.js updated with routes,middleware and session

--- server.js
+++ server.js
@@ -14,11 +14,27 @@ const port = process.env.PORT ? process.env.PORT : "3000"
 const methodOverride = require("method-override")
 const morgan = require("morgan")
 const session = require("express-session")
+const signedIn = require('./middleware/is-signed-in')
 
-// Run Middlewares
+// use Middlewares
 app.use(express.urlencoded());
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
+
+
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+
+const authRouter = require("./routes/auth")
+
+
+app.use('/auth', authRouter)
 
 
 app.listen(port, () => {
